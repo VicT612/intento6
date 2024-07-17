6568e4ec8292cb648b887cc54a553210c1dd9896
VicT612 <tadeov612@gmail.com>
2024-07-16 20:16:13 -0400

    corregido2

--- src/pages/api/libro/librito.ts
+++ src/pages/api/libro/librito.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '../../../lib/lib';
+import { db } from '../../../lib/lib'; // Ajusta la ruta según sea necesario
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
